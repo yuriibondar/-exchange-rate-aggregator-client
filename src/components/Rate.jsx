053bd83dc660328b51ec65cba3f5bd248fb9c4bf
Yurii Bondar <yuriybondar84@gmail.com>
2023-01-17 17:53:29 +0200

    Improved CSS

--- src/components/Rate.jsx
+++ src/components/Rate.jsx
@@ -2,14 +2,14 @@ import styles from './Rate.module.css';
 
 const Rate = (props) => {
   return (
-    <>
+    <div className={styles.container}>
       <div className={styles.name}>{props.name}</div>
       <div className={styles.exchangeRate}>
         <div className="buy">{props.buy?.toFixed(2)}</div>
         <div className="divider">/</div>
         <div className="sell">{props.sell?.toFixed(2)}</div>
       </div>
-    </>
+    </div>
   );
 };
 
