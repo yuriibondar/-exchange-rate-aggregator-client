0806bf8ea8a05d07dc6370de0e4ded02b20c86bf
Yurii Bondar <yuriybondar84@gmail.com>
2023-02-11 12:48:16 +0200

    Updated rates markup so that it is formatted correctly when copy-pasting text

--- client/src/components/Rate.jsx
+++ client/src/components/Rate.jsx
@@ -3,12 +3,12 @@ import styles from './Rate.module.css';
 const Rate = (props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.name}>{props.name}</div>
-      <div className={styles.exchangeRate}>
-        <div className="buy">{props.buy?.toFixed(2)}</div>
-        <div className="divider">/</div>
-        <div className="sell">{props.sell?.toFixed(2)}</div>
-      </div>
+      <span className={styles.name}>{props.name}&nbsp;</span>
+      <span className={styles.exchangeRate}>
+        <span className="buy">{props.buy?.toFixed(2)}</span>
+        <span className="divider">/</span>
+        <span className="sell">{props.sell?.toFixed(2)}</span>
+      </span>
     </div>
   );
 };
