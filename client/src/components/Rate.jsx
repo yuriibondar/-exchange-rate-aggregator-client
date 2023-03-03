c1cd3f4cd36e9334bd3b234b14f826b6f3951e20
Yurii Bondar <yuriybondar84@gmail.com>
2023-03-03 15:52:48 +0200

    Commented &nbsp;

--- client/src/components/Rate.jsx
+++ client/src/components/Rate.jsx
@@ -7,7 +7,7 @@ const Rate = (props) => {
   }
   return (
     <div className={styles.container}>
-      <span className={styles.name}>{props.name}&nbsp;</span>
+      <span className={styles.name}>{props.name}&nbsp;</span> {/* Adding non-breaking space in order to have a space between name and rate when copy-pasting from the page */}
       <span className={styles.exchangeRate}>
         <span className="buy">{formatRate(props.buy)}</span>
         <span className="divider">/</span>
