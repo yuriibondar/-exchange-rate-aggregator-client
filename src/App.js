fa85dd28b5cbd506c0499f7158dff63e2d924387
Yurii Bondar <yuriybondar84@gmail.com>
2023-05-08 15:39:27 +0300

    Analytics test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import './App.css'
-import RatesTable from './components/RatesTable';
-import { Analytics } from '@vercel/analytics/react';
+import "./App.css";
+import RatesTable from "./components/RatesTable";
+import { Analytics } from "@vercel/analytics/react";
 
-const App = ({state, fetchRates}) => {
-    return (
-        <div>
-            <header className='appHeader'>Моніторинг курсів</header>
-            <main className='mainContent'>
-                <RatesTable exchanges={state.exchanges} fetchRates={fetchRates} isLoading={state.isLoading} />
-                {/* <iframe src="https://obmennovosti.info/city.php?city=39" width="100%" height="500"></iframe> */}
-            </main>
-            <Analytics />
-        </div>
-    )
-}
+const App = ({ state, fetchRates }) => {
+  return (
+    <>
+      <div>
+        <header className="appHeader">Моніторинг курсів</header>
+        <main className="mainContent">
+          <RatesTable exchanges={state.exchanges} fetchRates={fetchRates} isLoading={state.isLoading} />
+          {/* <iframe src="https://obmennovosti.info/city.php?city=39" width="100%" height="500"></iframe> */}
+        </main>
+      </div>
+      <Analytics />
+      Analytics
+    </>
+  );
+};
 
 export default App;
